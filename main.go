8efed913d9b3e2ebdff6c4fdea224807bdf4a0e7
Mo <moho2010@live.ie>
2025-03-14 09:48:36 -0400

    first checkpoint reached

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	_ "encoding/json"
+	"fmt"
+	"log"
+	_ "math/rand"
+	"net/http"
+	_ "strconv"
+
 	"github.com/gorilla/mux"
 )
 
@@ -18,8 +25,22 @@ type Director struct {
 
 var movies []Movie
 
+func GetMovies(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	r := mux.NewRouter()
+	movies = append(movies, Movie{ID: "1", Isbn: "437556", Title: "Cherry Blossoms",
+		Director: &Director{Firstname: "Ruth", LastName: "Bourke"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "455676", Title: "Superman",
+		Director: &Director{Firstname: "Mahmed", LastName: "Ibrahim"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
-	r.HandleFunc()
+	fmt.Printf("starting server on localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
